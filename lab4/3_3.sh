0aafbcd05314406eeda0e2e9dd17c74fd4bf1b73
StudentMephi <55534805+StudentMephi@users.noreply.github.com>
2019-12-24 00:54:54 +0300

    тестирует заданный пользователем сценарий с разными входными даными

--- lab4/3_3.sh
+++ lab4/3_3.sh
@@ -2,4 +2,4 @@
 bash $1 "1" " 2" "3"
 bash $1 "foo" "bar" "foobar"  "foo bar"
 bash $1 "foo" "--foo" "--help" "-I"
-bash $1 "$RANDOM" "$RANDOM" "$RANDOM" "$RANDOM" "$RANDOM"
+bash $1 "$RANDOM" "$RANDOM" "$RANDOM" "$RANDOM" "$RANDOM" 
